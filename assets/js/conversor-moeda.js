a616556ae36b5f6463500a3b02440bce805f36fa
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-05-27 10:16:33 -0300

    Create conversor-moeda.js

---
+++ assets/js/conversor-moeda.js
@@ -0,0 +1,20 @@
+function convertCurrency() {
+    const amount = document.getElementById("amount").value;
+    const currency = document.getElementById("currency").value;
+
+    // Taxa de conversão aproximada em 27/05/2024
+    const exchangeRates = {
+        USD: 0.19, // 1 BRL = 0.19 USD
+        EUR: 0.18, // 1 BRL = 0.18 EUR
+        BTC: 0.0000028 // 1 BRL = 0,0000028 BTC
+    };
+
+    const result = amount * exchangeRates[currency];
+
+    document.getElementById('result').innerText = 'Valor convertido: ' + `${currency} ${result.toFixed(2)}`;
+}
+
+function limparCurrency() {
+    document.getElementById('amount').value = 0;
+    document.getElementById('result').textContent = 'Valor convertido: USD 0.00';
+}
