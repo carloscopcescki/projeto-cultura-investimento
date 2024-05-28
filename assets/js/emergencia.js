22ce7b0f9818c0e797c6af90a1d2ec8f3b81d2c4
Copcescki <carlos.copcescki@gmail.com>
2024-05-27 22:43:05 -0300

    Update e inserção de calculadora

---
+++ assets/js/emergencia.js
@@ -0,0 +1,23 @@
+function calcularReserva() {
+    var custoFixo = parseFloat(document.getElementById('custoFixo').value);
+    var guarda = parseFloat(document.getElementById('guarda').value);
+    var salarioMes = parseInt(document.getElementById('salarioMes').value);
+
+    var porcentagemMes = guarda / 100;
+    var poupancaMensal = salarioMes * porcentagemMes;
+    var reservaEmergencia = 6 * custoFixo;
+    var mesesCompletar = reservaEmergencia / poupancaMensal;
+
+    document.getElementById('reservaEmergencia').textContent = 'Reserva de emergência: R$ ' + reservaEmergencia.toFixed(2);
+    document.getElementById('guardando').textContent = 'Guardando: ' + guarda.toFixed(0).replace('.', ',');
+    document.getElementById('mesesCompletar').textContent = 'Meses para completar a reserva: ' + mesesCompletar.toFixed(0).replace('.', ',');
+}
+
+function limparCampos() {
+    document.getElementById('custoFixo').value = 0;
+    document.getElementById('guarda').value = 0;
+    document.getElementById('salarioMes').value = 1;
+    document.getElementById('reservaEmergencia').textContent = 'Reserva de emergência: R$ 0.00';
+    document.getElementById('mesesCompletar').textContent = 'Meses para completar a reserva: ';
+    document.getElementById('guardando').textContent = 'Guardando: ';
+}
